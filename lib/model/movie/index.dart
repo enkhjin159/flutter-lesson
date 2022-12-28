d289b1230d3e6b1a4a868943f438f2a843beab7e
B190920007@must.edu.mn Sict@007 <h.enkhjin324@gmail.com>
2022-12-28 18:09:35 +0800

    feat: Movie Model serialization done

--- lib/model/movie/index.dart
+++ lib/model/movie/index.dart
@@ -1,1 +1,25 @@
-class MovieModel {}
+import 'package:json_annotation/json_annotation.dart';
+
+part 'index.g.dart';
+
+@JsonSerializable()
+class MovieModel {
+  int id;
+  String imgUrl;
+  int? publishedYear, durationMin;
+  String? type, description;
+
+  MovieModel({
+    required this.id,
+    required this.imgUrl,
+    this.publishedYear,
+    this.durationMin,
+    this.type,
+    this.description,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) =>
+      _$MovieModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MovieModelToJson(this);
+}
