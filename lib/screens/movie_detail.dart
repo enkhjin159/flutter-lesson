652a26cfaa718bf8f88da760e9fcca4bb78c1726
B190920007@must.edu.mn Sict@007 <h.enkhjin324@gmail.com>
2023-01-02 14:43:43 +0800

    feat: global_key  done

--- lib/screens/movie_detail.dart
+++ lib/screens/movie_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie/model/movie/index.dart';
 import 'package:movie/providers/common.dart';
+import 'package:movie/utils/index.dart';
 import 'package:provider/provider.dart';
 
 class MovieDetailPage extends StatefulWidget {
@@ -72,7 +73,7 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
                                 ),
                                 SizedBox(height: 10),
                                 Text(
-                                  "${widget.data.publishedYear} | ${widget.data.durationMin} | ${widget.data.type}",
+                                  "${widget.data.publishedYear} | ${Utils.integerMinToString(widget.data.durationMin)} | ${widget.data.type}",
                                   style: TextStyle(
                                     fontSize: 14,
                                     color: Color(0xff777777),
